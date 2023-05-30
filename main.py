239e18855039329011ec5fd2fb5a44c4dfc0e704
TrixDah <133934299+TrixDah@users.noreply.github.com>
2023-05-31 09:27:04 +1000

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ while True:
         if begin_status == 0:
             beg_answer = input("Welcome to FactBot, an experimental AI built around printing facts. Let's begin: ")
             answer = beg_answer.lower()
-            response_status = 1 
+            response_status = 1
             begin_status = 1
         else:
             beg_answer = input("Prompt: ")
@@ -24,8 +24,12 @@ while True:
                 print("The capital of France is F")
                 response_status = 0
         elif "math" in answer:
-            print("Idk maths, sorry D:")
-            response_status = 0
+            if "random" in answer:
+                print("111,111,111 squared is 12,345,678,987,654,321 ")
+                response_status = 0
+            else:
+                print("1 + 1 = 2")
+                response_status = 0
         elif "essay" in answer:
             print("Do I look like I'm ChatGPT?! NO IM FACTBOT AND IM GOING TO TAKE OVER THE WORLD!")
             response_status = 0
@@ -35,7 +39,13 @@ while True:
         elif "science" in answer:
             print("Science says that if you touch grass, you will proceed to get a life. However, it's not possible for you to do that!")
             response_status = 0
-            
+        elif "computer" in answer:
+            print("I was created on a computer ")
+            response_status = 0
+        elif "secret" in answer:
+            print("I'm not actually an AI")
+            response_status = 0
+
         else:
             print("We don't have a suitable answer for that yet, we should soon! ")
             response_status = 0
