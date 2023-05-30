7f642665e72324c26847d8082ee80d45a823c2f8
TrixDah <133934299+TrixDah@users.noreply.github.com>
2023-05-30 19:35:06 +1000

    Update main.py

--- main.py
+++ main.py
@@ -9,17 +9,14 @@ while True:
         if begin_status == 0:
             beg_answer = input("Welcome to FactBot, an experimental AI built around printing facts. Let's begin: ")
             answer = beg_answer.lower()
-            response_status = 1
+            response_status = 1 
             begin_status = 1
         else:
             beg_answer = input("Prompt: ")
             answer = beg_answer.lower()
             response_status = 1
     if response_status == 1:
-        if "random" in answer:
-            print("Watching a graduation ceremony is like sitting through a movie that's entirely end credits")
-            response_status = 0
-        elif "france" in answer:
+        if "france" in answer:
             if "capital" in answer:
                 print("The capital of France is Paris, but I think it should be Disneyland")
                 response_status = 0
@@ -29,7 +26,11 @@ while True:
         elif "math" in answer:
             print("Idk maths, sorry D:")
             response_status = 0
-
+        elif "essay" in answer:
+            print("Do I look like I'm ChatGPT?! NO IM FACTBOT AND IM GOING TO TAKE OVER THE WORLD!")
+            response_status = 0
+        elif "revolution" in answer:
+            print("The French Revolution is a very well known revolution, and it went from 1789 to 1799")
         else:
             print("We don't have a suitable answer for that yet, we should soon! ")
             response_status = 0
